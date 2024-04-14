8de93ef2cae8774e9071d4b24f2468680530dcbe
kremilly <contact@kremilly.com>
2024-04-14 15:38:34 -0300

    feat: fix required on flag 'input'

--- ScanTOTP/__main__.py
+++ ScanTOTP/__main__.py
@@ -5,7 +5,7 @@ from classes.startup import Startup
 if __name__ == '__main__':
     Startup('E.g.: python scantotp -a qrcode -m file -i qrcode.png', [
         {'short': 'm', 'long': 'mode', 'help': 'Mode of scan', 'required': True},
-        {'short': 'i', 'long': 'input', 'help': 'Input of QR Code', 'required': True},
+        {'short': 'i', 'long': 'input', 'help': 'Input of QR Code', 'required': False},
         {'short': 'w', 'long': 'webcam', 'help': 'Select a webcam', 'default': '0', 'required': False},
         {'short': 'a', 'long': 'action', 'help': 'Action of runing', 'default': 'qrcode', 'required': True},
         {'short': 'c', 'long': 'copy', 'help': 'Copy of OTP code', 'action': 'store_true', 'required': False},
